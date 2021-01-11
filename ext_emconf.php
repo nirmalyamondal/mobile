bbdc5d8abe22d7cf525da8d99abce939b9ee0103
Nirmalya Mondal <nirmalya.mondal@gmail.com>
2021-01-11 20:30:21 +0530

    compatible for t3 version 11.

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.9.99',
+            'typo3' => '11.0.0-11.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
